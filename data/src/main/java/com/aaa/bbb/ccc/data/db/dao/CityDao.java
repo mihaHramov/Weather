9b91a909664b9526c317ea05bad147c3018016ff
Миша <mihail.hramov.1992@gmail.com>
2020-01-06 16:52:20 +0300

    and annotation "replaceIfExist"

--- data/src/main/java/com/aaa/bbb/ccc/data/db/dao/CityDao.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/db/dao/CityDao.java
@@ -2,12 +2,13 @@ package com.aaa.bbb.ccc.data.db.dao;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 
 import com.aaa.bbb.ccc.data.db.entity.City;
 
 @Dao
 public interface CityDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(City employee);
 }
