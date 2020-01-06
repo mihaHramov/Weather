a0bd30b424d359fad28d09b39e2361d1dcb6141c
Миша <mihail.hramov.1992@gmail.com>
2020-01-06 16:51:14 +0300

    update db table struct

--- data/src/main/java/com/aaa/bbb/ccc/data/db/entity/City.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/db/entity/City.java
@@ -6,10 +6,69 @@ import androidx.room.PrimaryKey;
 @Entity
 public class City {
     @PrimaryKey
-    public long id;
-    public String name;
-    public String country;
-    public int serverId;
-    public Double lat;
-    public Double lon;
+    private long id;
+    private String name;
+    private String country;
+    private Double latSin;
+    private Double latCos;
+    private Double lonSin;
+    private Double lonCos;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+
+    public Double getLatSin() {
+        return latSin;
+    }
+
+    public void setLatSin(Double latSin) {
+        this.latSin = latSin;
+    }
+
+    public Double getLatCos() {
+        return latCos;
+    }
+
+    public void setLatCos(Double latCos) {
+        this.latCos = latCos;
+    }
+
+    public Double getLonSin() {
+        return lonSin;
+    }
+
+    public void setLonSin(Double lonSin) {
+        this.lonSin = lonSin;
+    }
+
+    public Double getLonCos() {
+        return lonCos;
+    }
+
+    public void setLonCos(Double lonCos) {
+        this.lonCos = lonCos;
+    }
 }
