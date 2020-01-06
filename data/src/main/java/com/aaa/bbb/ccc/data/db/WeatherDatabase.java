ad5f78bc24f432871fe51169708b69060bc33e4b
Миша <mihail.hramov.1992@gmail.com>
2020-01-06 20:03:19 +0300

    extends "RoomDatabase"

--- data/src/main/java/com/aaa/bbb/ccc/data/db/WeatherDatabase.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/db/WeatherDatabase.java
@@ -1,11 +1,13 @@
 package com.aaa.bbb.ccc.data.db;
 
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 import com.aaa.bbb.ccc.data.db.dao.CityDao;
 import com.aaa.bbb.ccc.data.db.entity.City;
 
 @Database(entities = {City.class}, version = 1)
-public abstract  class WeatherDatabase {
+public abstract  class WeatherDatabase extends RoomDatabase {
+    public static final String NAME = WeatherDatabase.class.getName();
     public abstract CityDao getCityDao();
 }
