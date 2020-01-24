3bbbb23c53a2daca0ddeafda386280600a767590
Миша <mihail.hramov.1992@gmail.com>
2020-01-24 15:53:22 +0300

    add table to db

--- data/src/main/java/com/aaa/bbb/ccc/data/db/WeatherDatabase.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/db/WeatherDatabase.java
@@ -6,8 +6,9 @@ import androidx.room.RoomDatabase;
 import com.aaa.bbb.ccc.data.db.dao.CityDao;
 import com.aaa.bbb.ccc.data.db.dao.ForecastDao;
 import com.aaa.bbb.ccc.data.db.entity.City;
+import com.aaa.bbb.ccc.data.db.entity.Forecast;
 
-@Database(entities = {City.class}, version = 1)
+@Database(entities = {City.class, Forecast.class}, version = 1,exportSchema = false)
 public abstract  class WeatherDatabase extends RoomDatabase {
     public static final String NAME = WeatherDatabase.class.getName();
     public abstract CityDao getCityDao();
