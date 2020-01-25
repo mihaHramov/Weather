9fb91a290c0b927598692ced2f30b207ec036d2b
Миша <mihail.hramov.1992@gmail.com>
2020-01-26 01:04:15 +0300

    fix method "getWeatherForecast"

--- data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/WeatherForecastRepository.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/WeatherForecastRepository.java
@@ -1,14 +1,13 @@
 package com.aaa.bbb.ccc.data.repository.impl;
 
-import com.aaa.bbb.ccc.data.map.FromCityApiToCity;
 import com.aaa.bbb.ccc.data.map.FromResponseToListOfDetailedWeatherForecast;
+import com.aaa.bbb.ccc.data.map.FromWeatherResponseToWeatherForecast;
 import com.aaa.bbb.ccc.data.model.WeatherForecast;
 import com.aaa.bbb.ccc.data.network.OpenWeatherMapApi;
 import com.aaa.bbb.ccc.data.repository.intrf.ICashRepository;
 import com.aaa.bbb.ccc.data.repository.intrf.IWeatherForecastRepository;
 
 import rx.Observable;
-import rx.functions.Func1;
 
 public class WeatherForecastRepository implements IWeatherForecastRepository {
     private OpenWeatherMapApi mWeatherApi;
@@ -23,15 +22,8 @@ public class WeatherForecastRepository implements IWeatherForecastRepository {
     @Override
     public Observable<WeatherForecast> getWeatherForecast(String lat, String lon, String lang, String metric) {
         return mWeatherApi.getForecast(lat, lon, lang, metric)
-                .flatMap(new FromResponseToListOfDetailedWeatherForecast(),
-                        (weatherResponse, detailedWeatherForecasts) ->
-                                Observable.just(weatherResponse.getCity())
-                                        .map(new FromCityApiToCity())
-                                        .zipWith(Observable.just(detailedWeatherForecasts), WeatherForecast::new))
-                .flatMap((Func1<Observable<WeatherForecast>, Observable<WeatherForecast>>)
-                        weatherForecastObservable -> weatherForecastObservable)
-                //save forecast
+                .flatMap(new FromResponseToListOfDetailedWeatherForecast(), new FromWeatherResponseToWeatherForecast())
                 .doOnNext(weatherForecast -> mCashRepository.saveWeatherForecast(weatherForecast))
-                .onErrorResumeNext(mCashRepository.getWeatherForecast(lat, lon, lang, metric));
+                .onErrorResumeNext(throwable -> mCashRepository.getWeatherForecast(lat, lon, lang, metric));
     }
 }
