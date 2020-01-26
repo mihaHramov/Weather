a321f1c1cf130cfd74e6ef8a5d6bbe0c1d4df7e9
Миша <mihail.hramov.1992@gmail.com>
2020-01-26 15:35:24 +0300

    fix onErrorResumeNext

--- data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/CityRepository.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/CityRepository.java
@@ -5,14 +5,12 @@ import androidx.core.util.Pair;
 import com.aaa.bbb.ccc.data.map.TranslateLanguageMap;
 import com.aaa.bbb.ccc.data.map.ZipCityAndTranslateInfo;
 import com.aaa.bbb.ccc.data.model.City;
-import com.aaa.bbb.ccc.data.model.translateApi.TranslateResponse;
 import com.aaa.bbb.ccc.data.network.OpenWeatherMapApi;
 import com.aaa.bbb.ccc.data.network.TranslateApi;
 import com.aaa.bbb.ccc.data.repository.intrf.ICashRepository;
 import com.aaa.bbb.ccc.data.repository.intrf.ICityRepository;
 
 import rx.Observable;
-import rx.functions.Func1;
 
 public class CityRepository implements ICityRepository {
     private ICashRepository mCashRepository;
@@ -34,21 +32,19 @@ public class CityRepository implements ICityRepository {
         Observable<String> oldLanguage = language.map(new TranslateLanguageMap(OpenWeatherMapApi.BASE_LANGUAGE));
         Observable<City> defaultResult = Observable.zip(name, Observable.just(OpenWeatherMapApi.BASE_LANGUAGE), Pair::new)
                 .zipWith(cityObservable, mapper);
-
         Observable<City> resultOfTranslateObs = Observable.zip(name, oldLanguage, Pair::new)
-                .flatMap((Func1<Pair<String, String>, Observable<String>>)//translate
-                                stringStringPair ->
-                                        mTranslateApi.getTranslate(stringStringPair.first, stringStringPair.second)
-                                                .flatMap((Func1<TranslateResponse, Observable<String>>)
-                                                        translateResponse -> Observable.from(translateResponse.getText()))
-                                                .first(),
-                        (stringStringPair, s) -> new Pair<>(s, stringStringPair.second)
-                )
+                .flatMap(stringStringPair -> getTranslate(stringStringPair.first,stringStringPair.second),
+                        (stringStringPair, s) ->new Pair<>(s, stringStringPair.second))
                 .zipWith(cityObservable, mapper)
                 .doOnNext(city -> mCashRepository.saveCity(city));
         return resultOfTranslateObs
                 .mergeWith(cityDbObs)
                 .first()
-                .onErrorResumeNext(defaultResult);
+                .onErrorResumeNext(throwable -> defaultResult);
+    }
+    private Observable<String> getTranslate(String text, String lang) {
+        return mTranslateApi.getTranslate(text, lang)
+                .flatMap(translateResponse -> Observable.from(translateResponse.getText()))
+                .first();
     }
 }
