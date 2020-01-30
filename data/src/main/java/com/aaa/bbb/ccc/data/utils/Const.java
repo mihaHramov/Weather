85cdd08abae9037d78b5ca1c60936c5d78656aa4
Миша <mihail.hramov.1992@gmail.com>
2020-01-30 17:24:45 +0300

    create class "Const"

---
+++ data/src/main/java/com/aaa/bbb/ccc/data/utils/Const.java
@@ -0,0 +1,6 @@
+package com.aaa.bbb.ccc.data.utils;
+public final class Const {
+    public static final String TranslateApiBaseUrl = "https://translate.yandex.net/api/v1.5/tr.json/";
+    public static final String OpenWeatherMapApiBaseUrl = "http://api.openweathermap.org/data/2.5/";
+    public static final String OpenWeatherMapApiBaseLanguage = "en";
+}
