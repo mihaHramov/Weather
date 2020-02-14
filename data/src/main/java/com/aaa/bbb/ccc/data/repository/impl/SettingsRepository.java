c0580d99dae2af0253406df3ee2bfd92229bab45
Миша <mihail.hramov.1992@gmail.com>
2020-02-14 22:55:09 +0300

    delete mock language

--- data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/SettingsRepository.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/repository/impl/SettingsRepository.java
@@ -1,14 +1,16 @@
 package com.aaa.bbb.ccc.data.repository.impl;
 
-import com.aaa.bbb.ccc.data.repository.intrf.ISettingsRepository;
 import com.aaa.bbb.ccc.data.model.Location;
+import com.aaa.bbb.ccc.data.repository.intrf.ISettingsRepository;
+
+import java.util.Locale;
 
 import rx.Observable;
 
 public class SettingsRepository implements ISettingsRepository {
     @Override
     public Observable<String> getLanguage() {
-        return Observable.just("ru");
+        return Observable.just(Locale.getDefault().getLanguage());
     }
 
     @Override
@@ -18,6 +20,6 @@ public class SettingsRepository implements ISettingsRepository {
 
     @Override
     public Observable<Location> getDefaultLocation() {
-        return Observable.just(new Location("48.05","37.93"));
+        return Observable.just(new Location("48.05", "37.93"));
     }
 }
