b1a9425217f4750ae235c5f4aeece35236573a62
Миша <mihail.hramov.1992@gmail.com>
2020-02-22 22:34:26 +0300

    add test to LocationRepository

---
+++ data/src/test/java/com/aaa/bbb/ccc/data/LocationRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.aaa.bbb.ccc.data;
+
+import android.location.Location;
+
+import com.aaa.bbb.ccc.data.repository.impl.LocationRepository;
+import com.aaa.bbb.ccc.data.repository.intrf.ILocationRepository;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
+import rx.Observable;
+import rx.observers.TestSubscriber;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class LocationRepositoryTest {
+
+    @Test
+    public void testRepositoryAndMap() {
+        Double lat = 23.5678;
+        Double lon = 19.4788;
+        ReactiveLocationProvider mockReactiveLocationProvider = mock(ReactiveLocationProvider.class);
+        Location location = mock(Location.class);
+        when(location.getLatitude()).thenReturn(lat);
+        when(location.getLongitude()).thenReturn(lon);
+        when(mockReactiveLocationProvider.getLastKnownLocation()).thenReturn(Observable.just(location));
+        ILocationRepository locationRepository = new LocationRepository(mockReactiveLocationProvider);
+        TestSubscriber<com.aaa.bbb.ccc.model.Location> locationTestSubscriber = new TestSubscriber<>();
+        locationRepository.getCurrentLocation().subscribe(locationTestSubscriber);
+        locationTestSubscriber.assertCompleted();
+        locationTestSubscriber.assertNoErrors();
+        com.aaa.bbb.ccc.model.Location locationResult = locationTestSubscriber.getOnNextEvents().get(0);
+        Assert.assertEquals(lat.toString(), locationResult.getLat());
+        Assert.assertEquals(lon.toString(), locationResult.getLot());
+    }
+}
