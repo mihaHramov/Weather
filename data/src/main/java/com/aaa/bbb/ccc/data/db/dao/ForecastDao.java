d3cd32a3b6f3e25e6e2386c9bfd7919ffb938f9d
Миша <mihail.hramov.1992@gmail.com>
2020-02-28 00:15:51 +0300

    add method "getForecastByCityIdAndDate"

--- data/src/main/java/com/aaa/bbb/ccc/data/db/dao/ForecastDao.java
+++ data/src/main/java/com/aaa/bbb/ccc/data/db/dao/ForecastDao.java
@@ -3,11 +3,18 @@ package com.aaa.bbb.ccc.data.db.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.aaa.bbb.ccc.data.model.entity.Forecast;
+
+import java.util.List;
 
 @Dao
 public interface ForecastDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Forecast forecast);
+
+    @Query("SELECT * " +
+            "FROM forecast WHERE city_id = :id AND date>=:date; ")
+    List<Forecast> getForecastByCityIdAndDate(Integer id, Integer date);
 }
