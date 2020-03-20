47d95e5f89c8ef77f08599bfe4561a56c2681902
Миша <mihail.hramov.1992@gmail.com>
2020-03-21 00:28:24 +0300

    change item of shortForecast viewHolder

--- app/src/main/java/com/aaa/bbb/ccc/weather/presentation/vh/ShortForecastViewHolder.java
+++ app/src/main/java/com/aaa/bbb/ccc/weather/presentation/vh/ShortForecastViewHolder.java
@@ -14,31 +14,35 @@ import com.aaa.bbb.ccc.weather.presentation.adapter.BaseRecyclerViewAdapter;
 import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class ShortForecastViewHolder extends RecyclerView.ViewHolder implements BaseRecyclerViewAdapter.Binder<ShortForecast> {
     private ImageView mIcon;
-    private TextView mDate;
-    private TextView mWeatherType;
+    private TextView mDay;
+    private TextView mTime;
     private TextView mMaxTemperature;
     private TextView mMinTemperature;
 
     public ShortForecastViewHolder(@NonNull View view) {
         super(view);
         mIcon = view.findViewById(R.id.icon);
-        mDate = view.findViewById(R.id.date);
-        mWeatherType = view.findViewById(R.id.weatherType);
+        mDay = view.findViewById(R.id.day);
+        mTime = view.findViewById(R.id.time);
         mMaxTemperature = view.findViewById(R.id.max_temperature);
         mMinTemperature = view.findViewById(R.id.min_temperature);
     }
 
     @Override
     public void bind(ShortForecast dailyForecast) {
+        Calendar calendar = DateServices.getDateByInteger(dailyForecast.getDate());
         Picasso.get().load(dailyForecast.getWeatherType().getIcon()).into(mIcon);
-        mWeatherType.setText(dailyForecast.getWeatherType().getDescription());
+        String time = calendar.get(Calendar.HOUR)+":"+calendar.get(Calendar.MINUTE);
+        mTime.setText(time);
         String max = Double.toString(dailyForecast.getTemperature().getMax());
         String min = Double.toString(dailyForecast.getTemperature().getMin());
         mMaxTemperature.setText(max);
         mMinTemperature.setText(min);
-        mDate.setText(DateServices.getDateByInteger(dailyForecast.getDate()).get(Calendar.DAY_OF_WEEK));
+        String day = DateServices.getDateByInteger(dailyForecast.getDate()).getDisplayName(Calendar.DAY_OF_WEEK,Calendar.LONG, Locale.getDefault());
+        mDay.setText(day);
     }
 }
